5d9113c11e7202409cae426d14efd613e923b4a4
nagaike <1901102@s.asojuku.ac.jp>
2020-07-04 04:55:16 +0000

    second

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Mylist v-bind:title="message" v-on:result-event="appAction"/>
+    <hr>
+    <div>
+      <table v-html="log"></table>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Mylist from './components/Mylist.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Mylist
+  },
+  data: function(){
+    return{
+      message:"メモを入力してください",
+      result : [],
+    };
+  },
+
+  computed:{
+    log:function(){
+      var table = '<tr><th class = "head">my list</th></tr>';
+      for(var i in this.result){
+        table += '<tr><td>' + this.result[i] + '</td></th>';
+      }
+      return table;
+    }
+  },
+
+  created:function(){
+    var items = localStorage.getItem('log');
+    var logs = JSON.parse(items);
+    if(logs != null){
+      this.result = logs;
+    }
+  },
+
+  methods:{
+    appAction:function(exp,res){
+      this.result.unshift([exp,res]);
+      if(this.result.length > 5){
+        this.result.pop();
+      }
+      var log = JSON.stringify(this.result);
+      localStorage.getItem8('log',log);
+    }
   }
 }
 </script>
@@ -25,4 +63,20 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+tr td{
+  padding:5px;
+  border:1px solid gray;
+}
+
+tr th{
+  padding:5px;
+  border:1px solid gray; 
+}
+
+tr th.head{
+  background-color:black;
+  color:white;
+}
+
 </style>
---
+++ src/components/Mylist.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class = "list">
+        <h1>{{ title }}</h1>
+        <div>
+            <div>
+                <input type = "text" v-model = "input">
+            </div>
+                <button v-on:click = "doAction">
+                    クリック
+                </button>
+        </div>  
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Mylist',
+    props:{
+        title:String,
+    },
+
+    data:function(){
+        return {
+            input: 'Input Here',
+        };
+    },
+
+    methods: {
+        doAction: function(){
+            //input text add array
+            var arr = this.input.push();
+
+            while(arr[5] != ''){
+                var last = arr.pop();
+                //if arr[5] not empty,repeat move each item to front in arr
+                for(var n in arr){
+                    arr[n] = arr[n + 1];
+                }
+            }
+            //Last item in arr of array,insert to last 
+            arr = last.push()
+
+
+            //Call result-event on $emit
+            this.$emit('result-event',arr);
+        }
+    }
+
+}
+</script>
